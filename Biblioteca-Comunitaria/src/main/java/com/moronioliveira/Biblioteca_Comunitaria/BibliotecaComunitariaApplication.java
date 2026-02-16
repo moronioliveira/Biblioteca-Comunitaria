a3dbcde5d3ac5670e0e95ea9182bafbf255d90ac
Moroni <moronioliveira24@gmail.com>
2026-02-15 22:36:34 -0300

    Initial commit

---
+++ Biblioteca-Comunitaria/src/main/java/com/moronioliveira/Biblioteca_Comunitaria/BibliotecaComunitariaApplication.java
@@ -0,0 +1,13 @@
+package com.moronioliveira.Biblioteca_Comunitaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaComunitariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaComunitariaApplication.class, args);
+	}
+
+}
---
+++ Biblioteca-Comunitaria/src/test/java/com/moronioliveira/Biblioteca_Comunitaria/BibliotecaComunitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moronioliveira.Biblioteca_Comunitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaComunitariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
